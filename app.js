822cad5c864551a516186e990bb86887742b5f75
özkan demir <ozkanndemir2009@gmail.com>
2022-09-23 16:39:54 +0300

    js seçilen işlemler yapılıyor

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-.  
+const up = document.querySelector(".previous-display")
+const down = document.querySelector(".current-display")
+const buttons = document.querySelector(".buttons-container")
+
+buttons.addEventListener("click",()=>{
+    if(e.target.classList.contains("num")){
+
+    }
+    if (e.target.classList.contains("operator")) {
+
+    }
+    if (e.target.classList.contains("egual")) {
+
+    }
+    if (e.target.classList.contains("num")) {
+
+    }
+    if (e.target.classList.contains("num")) {
+
+    }
+
+})
